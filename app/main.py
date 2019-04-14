041ff97f8102761ae2a1ffef3c78cdb8aeb52dd7
Hao xu <haoxu@MacBook-Pro-3.T-mobile.com>
2019-04-14 15:33:42 -0700

    first init.

---
+++ app/main.py
@@ -0,0 +1,72 @@
+from bs4 import BeautifulSoup
+import feedparser
+from flask import Flask, render_template
+from flask_caching import Cache
+import requests
+import logging
+from logging import FileHandler, INFO
+from logging.config import dictConfig
+import os
+import json
+from PIL import Image
+import requests
+from io import BytesIO
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'file': {
+        'class': 'logging.FileHandler',
+        'filename': 'app.log',
+        'formatter': 'default',
+        'level': 'INFO'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['file']
+    }
+})
+
+app = Flask(__name__)
+cache = Cache(config={'CACHE_TYPE': 'simple'})
+cache.init_app(app)
+
+
+@app.route('/', methods=['GET'])
+def home_page():
+    feed_names = ['Awesome', 'DarkHumor', 'Funny']
+    return render_template('feeds.html', feed_names=feed_names)
+
+
+@app.route('/feeds/<feed_name>', methods=['GET'])
+@cache.cached(timeout=120)
+def show_feed(feed_name):
+    app.logger.info("request")
+    url = 'https://9gag-rss.com/api/rss/get?code=9GAG{}&format=1'.format(
+        feed_name)
+    feed_contents = feedparser.parse(url)
+    entries = feed_contents.entries
+    widths = list()
+    for entry in entries:
+        html_doc = entry['content'][0]['value']
+        soup = BeautifulSoup(html_doc, 'html.parser')
+        if soup.video:
+            width = soup.video['width']
+        elif soup.img:
+            src = soup.img['src']
+            response = requests.get(src)
+            img = Image.open(BytesIO(response.content))
+            width = '{}px'.format(str(img.size[0]))
+        else:
+            width = ""
+        widths.append(width)
+    # with open("./entries.json", "w") as fp:
+    #     json.dump(feed_contents.entries, fp, indent=4)
+    return render_template('content.html', feed_name=feed_name, entries=entries, widths=widths)
+
+
+api_port = int(os.environ.get("API_PORT", 700))
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=api_port, debug=True)
---
+++ app/test.py
@@ -0,0 +1,37 @@
+import os
+import unittest
+import time
+
+from main import app
+
+
+class BasicTests(unittest.TestCase):
+
+    def setUp(self):
+        self.client = app.test_client()
+
+    def tearDown(self):
+        pass
+
+    def test_main_page(self):
+        response = self.client.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_feed_Awesome_page(self):
+        time.sleep(5)
+        response = self.client.get('/feeds/Awesome', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_feed_DarkHumor_page(self):
+        time.sleep(5)
+        response = self.client.get('/feeds/DarkHumor', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_feed_Funny_page(self):
+        time.sleep(5)
+        response = self.client.get('/feeds/Funny', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
+    unittest.main()
